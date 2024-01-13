0fd4d817f92150e2b9cb081b8a429c8bf24ed12f
Hoanghuyen2k3 <khanhhuyenx20@gmail.com>
2024-01-13 17:06:36 -0500

    add chatbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,23 @@ import { FaVolumeUp, FaVolumeMute  } from "react-icons/fa";
 import Sweet from "./sound/Sweet.mp3";
 import {useState, useEffect} from "react";
 import Identification from './pages/identification/Identification'
+import ChatBox from "./components/Chatbox/Chatbox";
 function App() {
+        const firstMessage ="Hello, I'm a Tax Advisor, how can I help you today?";
+
+        const [messages, setMessages] = useState([firstMessage]);
+        const [inputText, setInputText] = useState('');
+        const [loading, setLoading] = useState(false); // Track loading state
+        const [show, setShow] = useState(false);
+        useEffect(() =>console.log(show), [show])
+      
+        const handleChat =(e)=>{
+          e.preventDefault();
+          setShow(show =>!show);
+          console.log(show)
+      
+        }
+
         const location = useLocation();
         const [play, setPlay] = useState(false);
 
@@ -41,6 +57,24 @@ function App() {
   return (
     <div className="App">
       <div className="main-audio" onClick={()=>setPlay((play)=>!play)}>{play ? <FaVolumeUp /> : <FaVolumeMute />} </div>
+      {
+            show ? <div className="chatbox">
+              <ChatBox 
+                messages={messages} 
+                setMessages={setMessages}
+                loading={loading}
+                setLoading={setLoading}
+                inputText={inputText}
+                setInputText={setInputText} 
+                setShow={setShow}/></div>:(
+              <div className="ai-img-container" onClick={handleChat}>
+                <img className="ai-img" src="https://play-lh.googleusercontent.com/Oe0NgYQ63TGGEr7ViA2fGA-yAB7w2zhMofDBR3opTGVvsCFibD8pecWUjHBF_VnVKNdJ=w240-h480-rw" alt="AI-chatbox" />
+
+              </div>
+
+            )
+          }
+
       <AnimatePresence mode='wait'>
         
       <Routes location={location} key={location.pathname} >
---
+++ frontend/src/components/Chatbox/Chatbox.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import './Chatbox.scss';
+import axios from 'axios';
+import { AiOutlineSend } from "react-icons/ai";
+import { BsArrowDownRight, BsXLg } from "react-icons/bs";
+
+
+const ChatBox = ({messages, setMessages, inputText, setInputText, loading, setLoading, setShow}) => {
+  const handleInputChange = event => {
+    setInputText(event.target.value);
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    if (inputText.trim() !== '') {
+      setMessages(prevMessages => [
+        ...prevMessages,
+        { text: inputText, isUser: true },
+      ]);
+
+      try {
+        setLoading(true); // Start loading
+        const response = await axios.post(
+          'https://memoritoo-server.onrender.com/vnhub',
+          {
+            message: inputText,
+          }
+        );
+
+        const aiResponse = response.data.answer;
+        setMessages(prevMessages => [
+          ...prevMessages,
+          { text: aiResponse, isUser: false },
+        ]);
+        setInputText(''); // Clear the input after sending
+      } catch (error) {
+        console.error('An error occurred:', error);
+      }
+      finally{
+        setLoading(false); // Stop loading
+      }
+    }
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="chat-nav">
+        <div onClick={(e)=>{
+          e.preventDefault();
+          setShow(false);
+
+        }}><BsArrowDownRight className="icon"/></div>
+        <div
+          onClick={(e)=>{
+            e.preventDefault();
+            setShow(false);
+            setMessages([])
+          }}
+        ><BsXLg className="icon"/></div> 
+      </div>
+      <div className="chat-messages">
+        {messages.map((message, index) => (
+          <div
+            key={index}
+            className={`message ${message.isUser ? 'user' : 'ai'}`}
+          >
+            {message.text}
+          </div>
+        ))}
+        {loading && (
+          <div className="message ai loading-indicator">
+            <span className="bounce b1">●</span>
+            <span className="bounce b2">●</span>
+            <span className="bounce b3">●</span>
+          </div>
+        )}
+      </div>
+      <form onSubmit={handleSubmit} className="chat-input-form">
+        <input
+          type="text"
+          value={inputText}
+          onChange={handleInputChange}
+          placeholder="Type your question..."
+          className="input-field"
+        />
+        <button type="submit" className="send-button">
+          <AiOutlineSend />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ChatBox;
