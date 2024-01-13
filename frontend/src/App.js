647c8f8ec5755eea82006bcfeb5ad4d86af6f410
Hoanghuyen2k3 <khanhhuyenx20@gmail.com>
2024-01-13 06:05:44 -0500

    add transition effect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
-
 import './App.css';
 import TypingEffect from './components/Typing/TypingEffect';
+import { AnimatePresence } from 'framer-motion';
+import { Routes, Route } from 'react-router-dom'; // Corrected import statement
+
 function App() {
   return (
     <div className="App">
-      <TypingEffect  />
+      <AnimatePresence mode='wait'>
+        <TypingEffect />
+      </AnimatePresence>
     </div>
   );
 }
